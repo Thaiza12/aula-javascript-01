46db32b2fbb1c3665b2620fb85a94902626009c1
Thaiza Alves <thaah.12@hotmail.com>
2021-09-23 20:56:34 -0300

    primeiro commit

---
+++ atividade01.js
@@ -0,0 +1,17 @@
+var nome= "Thaiza";
+console.log(nome);
+
+var nome="gabriel";
+console.log(nome);
+
+let idade_thaiza= 24;
+console.log(idade_thaiza);
+
+let idade_gabriel= 25;
+console.log(idade_gabriel);
+
+const pi=3.14159265359;
+console.log(pi)
+
+const nunber=28;
+console.log(nunber);
